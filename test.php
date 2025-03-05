5a52b550d8743de2cd598b54bcd90aeb8eccd37a
alwanzstore <latoskoko22@gmail.com>
2025-03-05 23:00:52 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alwanz</title>
+</head>
+<body>
+    <h1> Syafiq Alwan Syauqi </h1>
+</body>
+</html>
